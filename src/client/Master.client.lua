06502a2b5201a1e0e27ea130d25d45e573ad8b51
GlitchiPitch <nikitareutsky@gmail.com>
2024-10-18 09:57:11 +0500

    Create gui modules & initialize them

--- src/client/Master.client.lua
+++ src/client/Master.client.lua
@@ -1,9 +1,31 @@
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local StarterPlayer = game:GetService("StarterPlayer")
+
+local modules = StarterPlayer.StarterPlayerScripts.Modules
+
 local player = Players.LocalPlayer
 local playerGui = player.PlayerGui
 local playerScripts = player.PlayerScripts
 
+-- TYPES --
 local guiTypes = require(ReplicatedStorage.Types.Gui.Main)
 
+-- MODULES --
+local abilities = require(modules.Abilities)
+local controlPanel = require(modules.ControlPanel)
+local disableMovement = require(modules.DisableMovement)
+local progressBar = require(modules.ProgressBar)
+
 local mainGui = playerGui:WaitForChild("Main") :: guiTypes.Main
+
+function setup() end
+
+function init()  
+    abilities.init()
+    controlPanel.init()
+    disableMovement.init()
+    progressBar.init()
+end
+
+init()
---
+++ src/client/Modules/Abilities.lua
@@ -0,0 +1,6 @@
+function setup() end
+function init()  end
+
+return {
+    init = init,
+}
